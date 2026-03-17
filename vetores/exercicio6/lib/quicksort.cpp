cf17e4db8ced3bd138ba97138ac358069557e9ea
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-17 13:46:52 -0300

    feat: quicksort finished

--- vetores/exercicio6/lib/quicksort.cpp
+++ vetores/exercicio6/lib/quicksort.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 void swap(int &a, int &b) {
   int c = a;
   a = b;
@@ -11,8 +6,6 @@ void swap(int &a, int &b) {
 
 // Retorna o index do pivot no array
 int partition(int *arr, int start, int end) {
-  // TODO: código está quebrado, não sei porque. Arrumar
-  throw new exception;
   // Algorítmo de partição: utilizar dois ponteiros: um apontando a divisória
   // (j), outro iterando pelo array (i)
   // [22, 65, 7, 93, 4, 76, 29, 73, 89, 49]
@@ -20,17 +13,14 @@ int partition(int *arr, int start, int end) {
   int pivot = arr[end];
 
   for (int i = start; i <= end; ++i) {
-    // Evita os ponteiros de inverterem ou de uma troca ocorrer no mesmo
-    // elemento
-    if (i <= j + 1) {
-      continue;
-    }
+    if (arr[i] <= pivot) {
+      ++j;
 
-    if (arr[i] < pivot) {
-      swap(arr[++j], arr[i]);
+      if (i != j) {
+        swap(arr[j], arr[i]);
+      }
     }
   }
-  std::vector<int> dbg_vec(&arr[start], &arr[end]);
 
   // j agora está apontando para o pivot
   return j;
@@ -39,8 +29,8 @@ int partition(int *arr, int start, int end) {
 void quicksort(int *arr, int start, int end) {
   int pivot_pos;
 
-  // Caso base recursão
-  if (start >= end) {
+  // Caso base: array de tamanho 1 ou menor não precisa de sorting
+  if (end - start <= 0) {
     return;
   }
 
@@ -51,18 +41,4 @@ void quicksort(int *arr, int start, int end) {
   // Executar quick sort nas outras duas metades
   quicksort(arr, 0, pivot_pos - 1);
   quicksort(arr, pivot_pos + 1, end);
-}
-
-/*6-) Criar um vetor com a 8 elementos e ordená-los.*/
-int main() {
-  const int LEN = 8;
-  int vetor[LEN] = {0};
-
-  for (int i = 0; i < LEN; ++i) {
-    cout << "Digite o " << i + 1 << "° número: ";
-    cin >> vetor[i];
-  }
-
-  quicksort(vetor, 0, LEN - 1);
-  cout << vetor[0];
 }
---
+++ vetores/exercicio6/main.cpp
@@ -0,0 +1,21 @@
+#include "lib/quicksort.cpp"
+#include <iostream>
+
+using namespace std;
+
+/*6-) Criar um vetor com a 8 elementos e ordená-los.*/
+int main() {
+  const int LEN = 8;
+  int vetor[LEN] = {0};
+
+  for (int i = 0; i < LEN; ++i) {
+    cout << "Digite o " << i + 1 << "° número: ";
+    cin >> vetor[i];
+  }
+
+  quicksort(vetor, 0, LEN - 1);
+
+  for (auto v : vetor) {
+    cout << v << ' ';
+  }
+}
