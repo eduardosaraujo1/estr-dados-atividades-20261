6628556865666984fdb904e22ec25702f61217aa
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-24 15:33:01 -0300

    feat: insertion sort

--- vetores/exercicio6/lib/insertionsort.cpp
+++ vetores/exercicio6/lib/insertionsort.cpp
@@ -1,26 +1,16 @@
-void insert(int idx_src, int idx_dst, int *arr) {
-  // Esse algorítmo não foi projetado para
-  // quando o destino está a frente da fonte
-  if (idx_src >= idx_dst)
-    return;
-
-  int tmp = arr[idx_src];
-  for (int p = idx_src - 1; p >= idx_dst; --p) {
-    arr[p] = arr[p + 1];
-  }
-
-  arr[idx_dst] = tmp;
-}
-
 void insertionsort(int *arr, int len) {
   // Comparar do segundo valor em diante e ir verificando qual número deve ser
   // trocado
   // 20, 50, 79, 46, 100, 76, 70, 11, 19, 10
   for (int i = 1; i < len; ++i) {
-    for (int j = i - 1; j >= 0; --j) {
-      if (arr[i] < arr[j]) {
-        insert(i, j + 1, arr);
-      }
+    int cur = arr[i];
+    int j = i - 1;
+
+    while (arr[j] > cur && j >= 0) {
+      arr[j + 1] = arr[j];
+      --j;
     }
+
+    arr[j + 1] = cur;
   }
 }
