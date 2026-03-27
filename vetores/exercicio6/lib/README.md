bb72c84e4aafadcf4ce566fd85b01577706e2de7
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-03-27 14:47:07 -0300

    docs: partial shellsort

---
+++ vetores/exercicio6/lib/README.md
@@ -0,0 +1,14 @@
+# História do ShellSort
+
+WIP
+
+# Explicação do ShellSort
+
+WIP
+
+## Detalhes: Prova do teorema de não regressão do ShellSort
+
+<details>
+<summary>Clique aqui para expandir</summary>
+
+</details>
--- vetores/exercicio6/lib/shellsort.cpp
+++ vetores/exercicio6/lib/shellsort.cpp
@@ -1,3 +1,7 @@
-void shellsort(int *arr) {
-  //
+void insertion_sort_gap(int *arr, int len, int gap) {
+  // Executar insertion sort em todos os subarrays com espaço definido
+}
+
+void shellsort(int *arr, int len) {
+  // Chamar método para todos gaps do shellsort (algoritmo a ser definido)
 }
